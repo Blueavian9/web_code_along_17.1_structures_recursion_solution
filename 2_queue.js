4cd3a39202f97074bc34cf19cf71f2ae27f90a83
Ladrillo <gabriel@cabrejas.net>
2022-09-02 21:02:42 +0200

    initial

---
+++ 2_queue.js
@@ -0,0 +1,20 @@
+class queue {
+  constructor() {
+    this.data = []
+  }
+  enqueue(value) {
+    this.data.push(value)
+  }
+  dequeue() {
+    return this.data.shift()
+  }
+}
+
+// Test cases:
+const myQueue = new queue
+myQueue.enqueue(3)
+myQueue.enqueue(6)
+myQueue.enqueue(9)
+console.log(myQueue.dequeue()) // 3
+console.log(myQueue.dequeue()) // 6
+console.log(myQueue.dequeue()) // 9
---
+++ 3_linked_list_search.js
@@ -0,0 +1,26 @@
+class listNode {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+function search(head, value) {
+  if (head === null) {
+    return false
+  }
+  if (head.value === value) {
+    return true
+  }
+  return search(head.next, value)
+}
+
+// Test cases:
+const head = new listNode(3)
+head.next = new listNode(6)
+head.next.next = new listNode(9)
+// The linked list is 3 -> 6 -> 9 (3 is the head of the list)
+
+console.log(search(head, 3)) // true
+console.log(search(head, 0)) // false
+console.log(search(head, 9)) // true
