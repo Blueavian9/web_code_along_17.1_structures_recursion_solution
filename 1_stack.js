c499cd34552e28134ddb8d8048745f8852afd418
Cesar Aguilar <Blueavian9@gmail.com>
2023-06-27 22:14:56 -0700

    Initial commit

--- 1_stack.js
+++ 1_stack.js
@@ -1,21 +1,22 @@
+// stack
+
 class stack {
-  constructor() {
-    this.data = [] 
-}
-push(value) {
-  this.data.push(value)
-}
-pop(){ 
-  return this.data.pop()
+  constructor(value) {
+    this.data = [];
+  }
+  push(value) {
+    this.data.push(value);
+  }
+  pop() {
+    this.data.pop();
   }
 }
 
-// Test cases: 
-const myStack = new stack 
+// Test cases:
+const myStack = new stack();
 myStack.push(3);
 myStack.push(6);
-myStack.push(9);  
-console.log(myStack.pop()) // 9
-console.log(myStack.pop()) // 6
-console.log(myStack.pop()) // 3
-
+myStack.push(9);
+console.log(myStack.pop()); // 9
+console.log(myStack.pop()); // 6
+console.log(myStack.pop()); // 3
--- 3_linked_list_search.js
+++ 3_linked_list_search.js
@@ -1,4 +1,9 @@
-// Linked_list
+class listNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 
 class listNode {
   constructor(value) {
@@ -8,22 +13,20 @@ class listNode {
 }
 
 function search(head, value) {
-  if (head === null) {
+  if (head === value) {
     return false;
   }
   if (head.value === value) {
     return true;
   }
-  return searcht(head.next, value);
+  return search(head.next, value);
 }
 
-
-
 // Test cases:
-const head = new listNode(3)
+const head = new listNode(3);
 head.next = new listNode(6);
 head.next = new listNode(9);
 // The linked list is 3 --> 6 --> 9 (2 is the head of the list)
-console.log(search(head, 3)) // true
-console.log(search(head, 6)) // false
-console.log(search(head, 9)) // true 
+console.log(search(head, 3)); // true
+console.log(search(head, 6)); // false
+console.log(search(head, 9)); // true
