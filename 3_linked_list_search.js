0ddc37020afff0f2c91019baf66ec6fd88c89700
Cesar Aguilar <Blueavian9@gmail.com>
2023-06-28 03:17:06 -0700

    Initial commit

--- 3_linked_list_search.js
+++ 3_linked_list_search.js
@@ -5,18 +5,11 @@ class listNode {
   }
 }
 
-class listNode {
-  constructor(value) {
-    this.value = value;
-    this.next = null;
-  }
-}
-
 function search(head, value) {
-  if (head === value) {
+  if (head === null) {
     return false;
   }
-  if (head.value === value) {
+  if (head.next === value) {
     return true;
   }
   return search(head.next, value);
@@ -26,7 +19,6 @@ function search(head, value) {
 const head = new listNode(3);
 head.next = new listNode(6);
 head.next = new listNode(9);
-// The linked list is 3 --> 6 --> 9 (2 is the head of the list)
 console.log(search(head, 3)); // true
 console.log(search(head, 6)); // false
 console.log(search(head, 9)); // true
