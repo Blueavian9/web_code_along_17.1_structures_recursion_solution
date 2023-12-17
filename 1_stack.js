30d3c30ea24e211545a1199c70a580e6e331e4aa
Cesar Aguilar <Blueavian9@gmail.com>
2023-12-16 17:05:09 -0800

    initial commit

--- 1_stack.js
+++ 1_stack.js
@@ -16,27 +16,55 @@ console.log(arr); // [2, 3, 4]
 arr.unshift(0);
 console.log(arr); // [0, 2, 3, 4] */
 
+/*
+Using a JavaScript array, implement a stack.
+Hint: push/pop operations in a stack happen on 
+the same side.
+Picture a stack of pancakes
+--adding and removing of flapjacks always happens
+from the "top" of the stack. With an array,
+we can use visualize either the "left" or "right" 
+side of the array as the "top" of the stack,
+so there are two ways to implement this.
+Useful array methods:
+push: add an element to the right side
+unshift: add an element to the left side
+pop: remove an element from the right side
+shift: remove an element from the left side
+*/
+
+// Driver Task
 class stack {
-  
-  constructor() {}
-  push(value) {}
-  pop() {}
+  constructor() {
+    this.items = [];
+  }
+
+  push(element) {
+    this.items.push(element);
+  }
+
+  pop() {
+    return this.items.pop();
+  }
+
+  peek() {
+    return this.items[this.items.length - 1];
+  }
+
+  isEmpty() {
+    return this.items.length === 0;
+  }
+
+  size() {
+    return this.items.length;
+  }
 }
 
-// Test cases:
-const myStack = new stack();
-myStack.push(3);
-myStack.push(6);
-myStack.push(9);
-console.log(myStack.pop()); // 9
-console.log(myStack.pop()); // 6
-console.log(myStack.pop()); // 3
-
-// Using a JavaScript array, implement a stack.
-// Hint: push/pop operations in a stack happen on the same side. Picture a stack of pancakes -- adding and removing of flapjacks always happens from the "top" of the stack. With an array, we can use visualize either the "left" or "right" side of the array as the "top" of the stack, so there are two ways to implement this.
-
-// Useful array methods:
-// push: add an element to the right side
-// unshift: add an element to the left side
-// pop: remove an element from the right side
-// shift: remove an element from the left side
+// Test stack
+let myStack = new Stack();
+myStack.push(10);
+myStack.push(20);
+console.log(myStack.peek()); // Outputs 20
+console.log(myStack.size()); // Outputs 2
+myStack.pop();
+console.log(myStack.size()); // Outputs 1
