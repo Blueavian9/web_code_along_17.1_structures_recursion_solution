54276c793bd0a22bd5ba4faea12d0cfad0fbfdc4
Cesar Aguilar <Blueavian9@gmail.com>
2023-12-17 21:35:37 -0800

    initial commit

---
+++ JS-Algo-&-DSA's.js
@@ -0,0 +1,35 @@
+/* Big-O Notation: The worst case complexity of an algorithm is represented using the Big-O notation
+Big-O notation describes the complexity of an algorithm using algebraic terms
+it has two important characteristics
+    ** It is expressed in terms of the input
+    ** It focuses on the bigger picture without getting caught up in the minute details    
+
+Time complexity: 
+
+Example: 
+summation(4) = 10
+1 + 2 + 3 + 4 = 10
+
+Count the number of times
+a statement executes based
+on the input size
+*/
+
+function summation(n) {
+    let sum = 0; 
+    for (let i = l; i <= n; i++) {
+        sum += i; 
+    }
+    return sum; 
+}
+
+
+/* */
+
+/* */ 
+
+/* */ 
+
+/* */
+
+/* */
